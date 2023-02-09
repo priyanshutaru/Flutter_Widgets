f6133e80421d5ddaae7f0354b406039246146b54
LENV <technicalpriyanshutaru@gmail.com>
2023-02-09 16:03:29 +0530

    end of testing

--- lib/explorsubmenu.dart
+++ lib/explorsubmenu.dart
@@ -14,109 +14,348 @@ class _SubMenuExplorState extends State<SubMenuExplor> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("SubMenu Explor"),
+        title: Text("Our Packages"),
       ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 8),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    color: Colors.red,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      //color: Colors.red,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://wedarranger.com/wed_vendor/upload/indian-wedding-photography-indian-brides-accessories.jpg.webp"),
+                          fit: BoxFit.cover),
+                    ),
                   ),
                 ),
-              ),
-              // SizedBox(
-              //   width: 5,
-              // ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      Text(
-                        "Kritika Marrage Lawn",
-                        style: TextStyle(fontSize: 20, color: Colors.black),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Follow",
-                        style: TextStyle(
-                            color: Colors.blue[700],
-                            fontWeight: FontWeight.bold),
-                      ),
+                // SizedBox(
+                //   width: 5,
+                // ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
                       // ignore: prefer_const_literals_to_create_immutables
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      Icon(
-                        Icons.location_on,
-                        color: Colors.black,
-                      ),
-                      Text("Lucknow"),
-                    ],
-                  )
-                ],
-              ),
-
-              Icon(Icons.more_vert),
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-
-          Container(
-            height: 300,
-            // width: 400,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.red,
-          ),
-          // ignore: prefer_const_literals_to_create_immutables
-          Row(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(
-                  Icons.favorite_border,
-                  color: Colors.black,
+                      children: [
+                        Text(
+                          "Kritika Marrage Lawn",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Follow",
+                          style: TextStyle(
+                              color: Colors.blue[700],
+                              fontWeight: FontWeight.bold),
+                        ),
+                        // ignore: prefer_const_literals_to_create_immutables
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: Colors.black,
+                        ),
+                        Text("Lucknow"),
+                      ],
+                    )
+                  ],
                 ),
-              ),
-              Text("Like"),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(Icons.sms),
-              ),
-              Text("Coments"),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(Icons.share),
-              ),
-              Text("Share"),
-            ],
-          ),
-          // ignore: prefer_const_literals_to_create_immutables
-          Row(children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Text("100  Likes"),
+      
+                Icon(Icons.more_vert),
+              ],
             ),
-          ],)
-        ],
+            SizedBox(
+              height: 10,
+            ),
+      
+            Container(
+              height: 300,
+              // width: 400,
+              width: MediaQuery.of(context).size.width,
+              // color: Colors.red,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://wedarranger.com/wed_vendor/upload/DSC_6569.JPG"),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            // ignore: prefer_const_literals_to_create_immutables
+            Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.favorite_border,
+                    color: Colors.black,
+                  ),
+                ),
+                Text("Like"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.sms),
+                ),
+                Text("Coments"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.share),
+                ),
+                Text("Share"),
+              ],
+            ),
+            // ignore: prefer_const_literals_to_create_immutables
+            Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text("100  Likes"),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      //color: Colors.red,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://wedarranger.com/wed_vendor/upload/indian-wedding-photography-indian-brides-accessories.jpg.webp"),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+                // SizedBox(
+                //   width: 5,
+                // ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Text(
+                          "Kritika Marrage Lawn",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Follow",
+                          style: TextStyle(
+                              color: Colors.blue[700],
+                              fontWeight: FontWeight.bold),
+                        ),
+                        // ignore: prefer_const_literals_to_create_immutables
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: Colors.black,
+                        ),
+                        Text("Lucknow"),
+                      ],
+                    )
+                  ],
+                ),
+      
+                Icon(Icons.more_vert),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+      
+            Container(
+              height: 300,
+              // width: 400,
+              width: MediaQuery.of(context).size.width,
+              // color: Colors.red,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://wedarranger.com/wed_vendor/upload/DSC_6569.JPG"),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            // ignore: prefer_const_literals_to_create_immutables
+            Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.favorite_border,
+                    color: Colors.black,
+                  ),
+                ),
+                Text("Like"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.sms),
+                ),
+                Text("Coments"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.share),
+                ),
+                Text("Share"),
+              ],
+            ),
+            // ignore: prefer_const_literals_to_create_immutables
+            Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text("100  Likes"),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      //color: Colors.red,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://wedarranger.com/wed_vendor/upload/indian-wedding-photography-indian-brides-accessories.jpg.webp"),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+                // SizedBox(
+                //   width: 5,
+                // ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Text(
+                          "Kritika Marrage Lawn",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Follow",
+                          style: TextStyle(
+                              color: Colors.blue[700],
+                              fontWeight: FontWeight.bold),
+                        ),
+                        // ignore: prefer_const_literals_to_create_immutables
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: Colors.black,
+                        ),
+                        Text("Lucknow"),
+                      ],
+                    )
+                  ],
+                ),
+      
+                Icon(Icons.more_vert),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+      
+            Container(
+              height: 300,
+              // width: 400,
+              width: MediaQuery.of(context).size.width,
+              // color: Colors.red,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://wedarranger.com/wed_vendor/upload/DSC_6569.JPG"),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            // ignore: prefer_const_literals_to_create_immutables
+            Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.favorite_border,
+                    color: Colors.black,
+                  ),
+                ),
+                Text("Like"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.sms),
+                ),
+                Text("Coments"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.share),
+                ),
+                Text("Share"),
+              ],
+            ),
+            // ignore: prefer_const_literals_to_create_immutables
+            Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text("100  Likes"),
+                ),
+              ],
+            ),
+          ],
+        ),
+        
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: SubMenuExplor(),
+      home: MyForm1(),
     );
   }
 }
